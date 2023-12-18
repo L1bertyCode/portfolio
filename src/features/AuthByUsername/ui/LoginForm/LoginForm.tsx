3ac5d7cf96262e5dc40ff2a4cd1fc5131735a8e1
LC <libertycodeg@gmail.com>
2023-12-19 02:36:06 +0300

    i h, c lS,lS

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,10 +1,11 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { classNames } from "@/shared/lib/classNames/classNames";
-
 import s from "./LoginForm.module.scss";
 import { Input } from "@/shared/ui/Input/Input";
 import { Button } from "@/shared/ui/Button/Button";
+import { useDispatch } from "react-redux";
+import { loginActions } from "../../model/slice/loginSlice";
 
 interface LoginFormProps {
   className?: string;
@@ -13,6 +14,19 @@ interface LoginFormProps {
 export const LoginForm = memo((props: LoginFormProps) => {
   const { className } = props;
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const onChangeUsername = useCallback(
+    (value: string) => {
+      dispatch(loginActions.setUsername(value));
+    },
+    [dispatch]
+  );
+  const onChangePassword = useCallback(
+    (value: string) => {
+      dispatch(loginActions.setPassword(value));
+    },
+    [dispatch]
+  );
   return (
     <div
       className={classNames(s.loginForm, {}, [className])}
@@ -21,11 +35,16 @@ export const LoginForm = memo((props: LoginFormProps) => {
         autoFocus={true}
         label={t("Username")}
         className={s.input}
+        onChange={onChangeUsername}
       />
-      <Input label={t("Password")} className={s.input} />
+      <Input
+        label={t("Password")}
+        className={s.input}
+        onChange={onChangePassword}
+      />
       <Button
         className={s.loginBtn}
-        variant="clear-inverted"
+        variant="outline-inverted"
       >
         {t("Login")}
       </Button>
