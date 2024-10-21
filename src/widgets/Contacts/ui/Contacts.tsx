ded081e6e0bc36bae272e79de90c5cc35fd541a2
LC <libertycodeg@gmail.com>
2024-10-21 20:19:07 +0300

    ch w/Cntcts

--- src/widgets/Contacts/ui/Contacts.tsx
+++ src/widgets/Contacts/ui/Contacts.tsx
@@ -3,6 +3,8 @@ interface ContactsProps { };
 export const Contacts = ({ }: ContactsProps) => {
   return (
     <div className={s.Contacts}>
+      <h2>For any questions please mail me:</h2>
+      <h3>libertycodeg@gmail.com</h3>
       <div>Contacts</div>
     </div>
   );
